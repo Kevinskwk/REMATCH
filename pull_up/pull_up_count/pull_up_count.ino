2eab7d58597ac29edbbed6812b02875ddf2bf36d
Kevinskwk <kevinskwk@gmail.com>
2019-11-23 23:23:11 +0800

    Implement multiple ultrasonic sensors

--- pull_up/pull_up_count/pull_up_count.ino
+++ pull_up/pull_up_count/pull_up_count.ino
@@ -1,21 +1,30 @@
-int echoPin = 10;
-int trigPin = 7;
-int LED = 8;
-bool cooldown = false;
-long duration;
-float distance;
+#include <hcsr04.h>
+
+#define NUM 2
+#define MAX_DISTANCE 2000  //in mm
+#define MIN_DISTANCE 20  //in mm
+#define INTERVAL 30  //in millisecond
+#define LIMIT 400 //in mm
+
+int LED = 6;
+bool cooldown = false; 
+unsigned int distance[NUM];
+
+HCSR04 sensor[NUM] = {
+  HCSR04(3, 2, MIN_DISTANCE, MAX_DISTANCE),
+  HCSR04(5, 4, MIN_DISTANCE, MAX_DISTANCE)
+};
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(echoPin, INPUT);
-  pinMode(trigPin, OUTPUT);
   pinMode(LED, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  distance = ultrasonic_distance();
+  //update_distance();
+  //Serial.println(distance[0]);
+  //Serial.println(distance[1]);
+
   if (debounce() && !cooldown) {
     Serial.println("ONE");
     cooldown = true;
@@ -27,34 +36,36 @@ void loop() {
   }
 }
 
-float ultrasonic_distance(){
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  return (duration*0.017);
+void update_distance() {
+  for (uint8_t i = 0; i < NUM; i++) {
+    delay(INTERVAL);
+    distance[i] = sensor[i].distanceInMillimeters();
+  }
 }
 
 bool debounce(){
-  distance = ultrasonic_distance();
-  if (distance <= 40) {
-    delay(100);
-    if (distance <=40) {
-      return true;
-    }
+  update_distance();
+  for (uint8_t i = 0; i < NUM; i++) {
+    if (distance[i] > 500) return false;
   }
-  return false;
+  delay(50);
+  update_distance();
+  for (uint8_t i = 0; i < NUM; i++) {
+    if (distance[i] > 500) return false;
+  }
+  return true;
 }
 
-bool debounce_inverted() {
-  distance = ultrasonic_distance();
-  if (distance > 40 && distance <= 200) {
-    delay(100);
-    if (distance >40 && distance <= 200) {
-      return true;
-    }
+
+bool debounce_inverted(){
+  update_distance();
+  for (uint8_t i = 0; i < NUM; i++) {
+    if (distance[i] <= 500) return false;
   }
-  return false;
+  delay(50);
+  update_distance();
+  for (uint8_t i = 0; i < NUM; i++) {
+    if (distance[i] <= 500) return false;
+  }
+  return true;
 }
